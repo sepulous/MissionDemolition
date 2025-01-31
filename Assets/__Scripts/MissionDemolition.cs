e0abeece101cf18674d004ff1bc200b2fca4cac7
Blake Abernathy <minerabacuss@gmail.com>
2025-01-30 20:11:58 -0600

    Created base game

---
+++ Assets/__Scripts/MissionDemolition.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+    idle,
+    playing,
+    levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour
+{
+    static private MissionDemolition S;
+
+    public Text uitLevel;
+    public Text uitShots;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
+
+    void Start()
+    {
+        S = this;
+        level = 0;
+        shotsTaken = 0;
+        levelMax = castles.Length;
+        StartLevel();
+    }
+
+    void Update()
+    {
+        UpdateGUI();
+
+        if (mode == GameMode.playing && Goal.goalMet)
+        {
+            mode = GameMode.levelEnd;
+            FollowCam.SWITCH_VIEW(FollowCam.eView.both);
+            Invoke("NextLevel", 2f);
+        }
+    }
+
+    void StartLevel()
+    {
+        if (castle != null)
+            Destroy(castle);
+
+        Projectile.DESTROY_PROJECTILES();
+
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+
+        Goal.goalMet = false;
+
+        UpdateGUI();
+
+        mode = GameMode.playing;
+
+        FollowCam.SWITCH_VIEW(FollowCam.eView.both);
+    }
+
+    void NextLevel()
+    {
+        level++;
+        if (level == levelMax)
+        {
+            level = 0;
+            shotsTaken = 0;
+        }
+        StartLevel();
+    }
+
+    void UpdateGUI()
+    {
+        uitLevel.text = $"Level: {level + 1} of {levelMax}";
+        uitShots.text = $"Shots Taken: {shotsTaken}";
+    }
+
+    static public void SHOT_FIRED()
+    {
+        S.shotsTaken++;
+    }
+
+    static public GameObject GET_CASTLE()
+    {
+        return S.castle;
+    }
+}
---
+++ Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject projLinePrefab;
+    public GameObject projectilePrefab;
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    public float velocityMult = 10f;
+
+    void Awake()
+    {
+        launchPoint.SetActive(false);
+        launchPos = launchPoint.transform.position;
+    }
+
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+            projRB.isKinematic = false;
+            projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.SWITCH_VIEW(FollowCam.eView.slingshot);
+            FollowCam.POI = projectile;
+            Instantiate<GameObject>(projLinePrefab, projectile.transform);
+            projectile = null;
+            MissionDemolition.SHOT_FIRED();
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
